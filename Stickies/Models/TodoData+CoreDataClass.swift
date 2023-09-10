36deede2c64eb78823873152e2872061b7e7f126
Dowon Kim <dowoniscool@yahoo.com>
2023-09-10 21:01:37 +0200

    Initial Commit - Original Project

---
+++ Stickies/Models/TodoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoData+CoreDataClass.swift
+//  Stickies
+//
+//  Created by Dowon Kim on 03/09/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoData)
+public class TodoData: NSManagedObject {
+
+}
